e546f8f8cc8057ad8abd0c385824bd6b673b3451
Sergey Dianov <sergik5849@gmail.com>
2023-10-20 20:43:47 +0300

    Create-react-app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import HeaderContainer from "./components/Containers/HeaderContainer";
+
+function App() {
+  return (
+    <>
+      <HeaderContainer>
+        123
+      </HeaderContainer>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Containers/HeaderContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './Containers.module.scss'
+const HeaderContainer = () => {
+    return (
+        <div className={styles.container}></div>
+
+    );
+};
+
+export default HeaderContainer;
